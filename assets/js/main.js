54a8bc40e739d7c549d18eac4847c47f1419ae08
candye_sweet <olya.melska@gmail.com>
2019-03-03 19:21:04 -0500

    Test New Styles

---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+let backgroundArray = [
+  "../images/background1.jpg",
+  "../images/background2.jpg",
+  "../images/background3.jpg",
+  "../images/background4.jpg"
+];
+let city = "New York";
+let satrtDate;
+let endDate;
+let APIkeyNews = "rVruAxBlYzZYu9EleoRs0Vbwvuo0NZ6c";
+let queryURLNews =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:Culture&Arts&Museums&glocations:NEW+YORK+CITY&sort=newest&api-key=" +
+  APIkeyNews;
+
+let APIkeyWeather = "70c19500496f9aecbd97ff861d3072ee";
+let queryURLWeather =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&units=imperial&appid=" +
+  APIkeyWeather;
+
+$.ajax({
+  url: queryURLNews,
+  method: "GET"
+}).then(function(news) {
+  console.log(queryURLNews);
+
+  console.log(news);
+  let response = news.response;
+  for (let i = 0; i < response.docs.length; i++) {
+    let cardDivNews = $("<div>").attr("class", "card");
+    let imgNews = $("<img>")
+      .attr(
+        "src",
+        "https://static01.nyt.com/" + response.docs[i].multimedia[0].url
+      )
+      .attr("class", "img-fluid");
+    let cardBodyDivNews = $("<div>").attr("class", "card-body");
+    let cardTitleNews = $("<h5>")
+      .text(response.docs[i].headline.main)
+      .attr("class", "card-title");
+    let cardTextNews = $("<p>")
+      .text(response.docs[i].snippet)
+      .attr("class", "card-text");
+
+    let linkNews = $("<a>")
+      .attr("href", response.docs[i].web_url)
+      .attr("class", "btn btn-dark btn-news")
+      .text("Read Article");
+    cardBodyDivNews.append(cardTitleNews, cardTextNews, linkNews);
+    cardDivNews.append(imgNews, cardBodyDivNews);
+    $("#displayNews").append(cardDivNews);
+  }
+});
+
+$.ajax({
+  url: queryURLWeather,
+  method: "GET"
+}).then(function(response) {
+  let temperatureAndCity = $("<p>").text(
+    response.name.toUpperCase() + ": " + response.main.temp + "° F "
+  );
+  temperatureAndCity.attr("class", "displayTemp");
+  let arr = [];
+  for (let i = 0; i < response.weather.length; i++) {
+    arr.push(response.weather[i].icon);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let imgWeather = $("<img>").attr(
+      "src",
+      "http://openweathermap.org/img/w/" + arr[i] + ".png"
+    );
+    imgWeather.css("filter", "grayscale(100%)");
+    $("#weather").append(imgWeather);
+  }
+  $("#weather").append(temperatureAndCity);
+});
+
+function signInExistingUser() {}
+
+function signUpNewUser() {}
+
+function continueAsGuest() {}
+
+function logout() {}
